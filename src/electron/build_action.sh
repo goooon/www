e76a95334cd67c8d2c58b429b9174008871d027e
goooon <goooon@126.com>
2019-01-03 13:30:42 +0800

    2

--- src/electron/build_action.sh
+++ src/electron/build_action.sh
@@ -14,6 +14,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+chmod 777 src/www/build_action.sh
+chmod 777 src/electron/*
+
 yarn do src/www/build
 
 tsc -p src/electron
